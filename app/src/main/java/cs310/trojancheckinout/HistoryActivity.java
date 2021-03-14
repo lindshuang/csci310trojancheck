860b919929f272abe46baa0ecf5c619831e296d3
Lindsay Huang <huan981@usc.edu>
2021-03-13 21:50:13 -0600

    pushed sample user
    finally was able to connect to the database

--- app/src/main/java/cs310/trojancheckinout/HistoryActivity.java
+++ app/src/main/java/cs310/trojancheckinout/HistoryActivity.java
@@ -7,7 +7,20 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Build;
 import android.os.Bundle;
 
+
 import java.time.LocalTime;
+
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import androidx.annotation.NonNull;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import cs310.trojancheckinout.models.History;
 
@@ -46,5 +59,21 @@ public class HistoryActivity extends AppCompatActivity {
         HistoryAdapter adapter = new HistoryAdapter(Histories);
 
         list.setAdapter(adapter);
+
+        String str = "test_string";
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        History new_history = new History("10:00", "12:00", "12:00", "12:00", 3.0, "Taper");
+        //String history_id = Integer.toString(current_student.getHistories().size()) + current_student.getEmail();
+        db.collection("history").document("1nutakki@usc.edu").set(new_history);
+
+
+        //FirebaseFirestore db = FirebaseFirestore.getInstance();
+        //db.collection("users").document("test_doc").set(str);
+
+//        db.
+//        db.collection("test").document("LA").set(str);
+//        DatabaseReference myRef = database.getReference("message");
+//        myRef.setValue("Hello, World!");
     }
 }
